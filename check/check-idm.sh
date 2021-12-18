f2992f960b11a4809aa16dbb6d5e0478efd164bb
JaimeZeng <zxj@outlook.sg>
2021-12-18 20:47:55 +0800

    idm: add idm check version script
    Signed-off-by: JaimeZeng <zxj@outlook.sg>

--- check/check-idm.sh
+++ check/check-idm.sh
@@ -5,8 +5,6 @@ export PATH
 
 DATE="$(echo $(TZ=UTC date '+%Y-%m-%d %H:%M:%S'))"
 tmpFile="./action.tmp"
-user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-aria2c_options='--quiet=true --max-connection-per-server=8 --retry-wait=20 --timeout=20 --check-certificate=false --allow-overwrite=true -U "${user_agent}"'
-
-aria2c ${aria2c_options} 'https://pan.jiemi.workers.dev/?file=/Public/scoop/IDM/sha256sum.txt'
+userAgent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+wget --user-agent="${userAgent}" -O 'sha256sum.txt' 'https://pan.jiemi.workers.dev/?file=/Public/scoop/IDM/sha256sum.txt'
 mv -f sha256sum.txt ../IDM
